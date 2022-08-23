ca6751e073aa321234d4cf321db1ee176c583597
Yogesh Kadam <y.b.kadam01@gmail.com>
2022-08-24 01:19:13 +0530

    Updated Diff.java

--- Diff.java
+++ Diff.java
@@ -1,5 +1,8 @@
 public class Diff {
     public static void main(String[] args) {
-        
+        int a = 4;
+        int b = 6;
+
+        System.out.println("The Difference is : " + a - b);
     }
 }
